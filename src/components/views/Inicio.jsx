cf9969f319befded66968787457d8058ceff4a94
Ivan Silva <silvaivan280@gmail.com>
2024-12-03 20:42:09 -0300

    agregue el maquetado de sobre nosotros

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -4,6 +4,7 @@ import iconoSistema from "../../../public/desarrollo-sistemas.png";
 import iconoMantenimiento from "../../../public/mantenimiento-web.png";
 import iconoSeo from "../../../public/seo.png";
 import iconoIdea from "../../../public/idea.png";
+import imgSobreNosotros from "../../../public/sobre-nosotros.webp";
 
 const Inicio = () => {
   return (
@@ -27,75 +28,121 @@ const Inicio = () => {
         <Row className="mt-5 justify-content-center">
           <Col md={4} className="d-flex justify-content-center">
             <Card className="card-shadow">
-              <Card.Img className="icono-servicio" variant="top" src={iconoWeb} />
+              <Card.Img
+                className="icono-servicio"
+                variant="top"
+                src={iconoWeb}
+              />
               <Card.Body>
                 <Card.Title>Desarrollo Web</Card.Title>
                 <Card.Text>
-                Transformamos tu idea en una presencia digital efectiva.
+                  Transformamos tu idea en una presencia digital efectiva.
                 </Card.Text>
                 <div className="text-center">
-                    <Button className="btn-info">Ver más</Button>
+                  <Button className="btn-info">Ver más</Button>
                 </div>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center">
             <Card className="card-shadow">
-              <Card.Img className="icono-servicio" variant="top" src={iconoSistema} />
+              <Card.Img
+                className="icono-servicio"
+                variant="top"
+                src={iconoSistema}
+              />
               <Card.Body>
-                <Card.Title>Sistemas y Aplicaciones Web Personalizadas</Card.Title>
+                <Card.Title>
+                  Sistemas y Aplicaciones Web Personalizadas
+                </Card.Title>
                 <Card.Text>
-                Creamos herramientas digitales que impulsan tu productividad.
+                  Creamos herramientas digitales que impulsan tu productividad.
                 </Card.Text>
                 <div className="text-center">
-                    <Button className="btn-info">Ver más</Button>
+                  <Button className="btn-info">Ver más</Button>
                 </div>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center">
             <Card className="card-shadow">
-              <Card.Img className="icono-servicio" variant="top" src={iconoMantenimiento} />
+              <Card.Img
+                className="icono-servicio"
+                variant="top"
+                src={iconoMantenimiento}
+              />
               <Card.Body>
                 <Card.Title>Mantenimiento</Card.Title>
                 <Card.Text>
-                Cuida tu página web, nosotros nos encargamos del resto.
+                  Cuida tu página web, nosotros nos encargamos del resto.
                 </Card.Text>
                 <div className="text-center">
-                    <Button className="btn-info">Ver más</Button>
+                  <Button className="btn-info">Ver más</Button>
                 </div>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center mt-5">
             <Card className="card-shadow">
-              <Card.Img className="icono-servicio" variant="top" src={iconoSeo} />
+              <Card.Img
+                className="icono-servicio"
+                variant="top"
+                src={iconoSeo}
+              />
               <Card.Body>
                 <Card.Title>Optimización y SEO</Card.Title>
                 <Card.Text>
-                Haz que tu página web sea la primera opción en Google.
+                  Haz que tu página web sea la primera opción en Google.
                 </Card.Text>
                 <div className="text-center">
-                    <Button className="btn-info">Ver más</Button>
+                  <Button className="btn-info">Ver más</Button>
                 </div>
               </Card.Body>
             </Card>
           </Col>
           <Col md={4} className="d-flex justify-content-center mt-5">
             <Card className="card-shadow">
-              <Card.Img className="icono-servicio" variant="top" src={iconoIdea} />
+              <Card.Img
+                className="icono-servicio"
+                variant="top"
+                src={iconoIdea}
+              />
               <Card.Body>
                 <Card.Title> Asesoramiento y Consultoría Digital</Card.Title>
                 <Card.Text>
-                Te ayudamos a construir el futuro digital de tu negocio.
+                  Te ayudamos a construir el futuro digital de tu negocio.
                 </Card.Text>
                 <div className="text-center">
-                    <Button className="btn-info">Ver más</Button>
+                  <Button className="btn-info">Ver más</Button>
                 </div>
               </Card.Body>
             </Card>
           </Col>
         </Row>
+        <section className="mt-5">
+          <article className="text-center">
+            <h2>Sobre nosotros</h2>
+            <hr />
+          </article>
+          <Row className="align-items-center justify-content-center">
+            <Col md={6} className="d-flex justify-content-center">
+              <p className="sobre-nosotros">
+                En Mate, nuestro objetivo es ofrecer soluciones digitales de
+                alta calidad que ayuden a las empresas a crecer en el entorno
+                digital. Creamos páginas web, sistemas personalizados y
+                aplicaciones que optimizan la productividad y mejoran la
+                presencia online de nuestros clientes.
+              </p>
+            </Col>
+            <Col md={6} className="d-flex justify-content-center">
+              <Card.Img
+                className="img-sobre-nosotros"
+                variant="top"
+                src={imgSobreNosotros}
+              />
+            </Col>
+          </Row>
+        </section>
       </Container>
     </main>
   );
