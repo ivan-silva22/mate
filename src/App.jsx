eff1bdc4f9a678e8022339f36bc0cc36f4400c2c
Ivan Silva <silvaivan280@gmail.com>
2024-12-04 10:42:21 -0300

    agregue el maquetado del footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './components/common/Menu'
 import Inicio from './components/views/Inicio';
+import Footer from './components/common/Footer';
 
 function App() {
 
   return (
     <>
      {/* <Menu/> */}
-     <Inicio/>
+     {/* <Inicio/> */}
+     <Footer/>
     </>
   )
 }
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,42 @@
+import { Row, Col } from "react-bootstrap";
+import logo from "../../../public/Mate.png";
+import facebook from "../../../public/facebook.png";
+import linkedin from "../../../public/linkedin.png";
+
+const Footer = () => {
+  return (
+    <footer className="shadow-lg p-3 mb-5 rounded">
+      <Row className="justify-content-center text-center ">
+        <Col md={6}>
+          <img className="img-logo-footer" src={logo} alt="logo de Mate" />
+        </Col>
+        <Col md={6} className="mt-2">
+          <div className="redes-sociales mt-4">
+            <a className="nav-link" href="">
+              <img
+                className="icono-redes"
+                src={facebook}
+                alt="logo de facebook"
+              />
+            </a>
+            <a className="nav-link" href="">
+              <img className="icono-redes" src={linkedin} alt="logo linkedin" />
+            </a>
+          </div>
+          <div className="mt-4">
+            <p className="footer-texto">contactomate@gmail.com</p>
+          </div>
+        </Col>
+      </Row>
+      <Row className="text-center mt-2">
+        <Col>
+          <p className="footer-texto">
+            © 2024 Mate | Todos los derechos reservados.
+          </p>
+        </Col>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
